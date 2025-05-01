ebf8f67443f9ec079e03085449382d7e24df5fe5
vinecnttsai <105853560+vinecnttsai@users.noreply.github.com>
2025-05-02 00:21:39 +0800

    Update tb_top_module.v

--- Q2/tb_top_module.v
+++ Q2/tb_top_module.v
@@ -28,7 +28,6 @@ module tb_top_module ();
         .AN(AN)
     );
 
-    // 時脈產生器：100MHz
     initial sys_clk = 0;
     always #5 sys_clk = ~sys_clk;
 
@@ -36,37 +35,31 @@ module tb_top_module ();
         sys_rst_n = 1;
         {E, F, G} = 3'b111;
         #3 sys_rst_n = 0;
-        #3 sys_rst_n = 1; // 先復位，然後釋放按鍵
+        #3 sys_rst_n = 1;
 
-        // 模擬按下 G 鍵（E=1, F=1, G=0），對應 3'b110
         #50;
         press_key(3'b110);
 
-        // 模擬 bouncing
         #30;  bounce_key(3'b110, 5);
 
-        // 再按一次不同鍵
         #300;
-        press_key(3'b101); // 對應 3'b101
+        press_key(3'b101);
 
         #50;
         bounce_key(3'b101, 4);
 
-        // 測試結束
         #1000;
         $finish;
     end
 
-    // 任意按鍵模擬
     task press_key(input [2:0] key);
         begin
             {E, F, G} = key;
             #100;
-            {E, F, G} = 3'b111; // 鬆開
+            {E, F, G} = 3'b111;
         end
     endtask
 
-    // 模擬 bouncing（多次重複快速跳動）
     task bounce_key(input [2:0] key, input integer bounce_times);
         integer i;
         begin
